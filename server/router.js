f6abe86f772d68376419674d7e544a881e6bc5f3
TDSOJohn <giovanni.basso@icloud.com>
2021-12-04 11:48:02 +0100

    added new paths to routes

--- server/router.js
+++ server/router.js
@@ -2,10 +2,19 @@ var express = require('express')
 var app = express()
 
 var terms = require('./routes/terms.js')
-app.use('/', terms)
+app.use('/terms', terms)
 
 var streaming = require('./routes/streaming.js')
 app.use('/stream', streaming)
+
+var maps = require('./routes/maps.js')
+app.use('/map', maps)
+
+var index = require('./routes/index.js')
+app.use('/index', index)
+
+var users = require('./routes/users.js')
+app.use('/users', users)
 
 const http = require('http')
 const server = http.createServer(app)
