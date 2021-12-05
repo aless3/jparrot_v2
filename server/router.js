76baa7a0cad054ed4b8912e1e663ebd4506d3050
aless <alessandro.benassi7@studio.unibo.it>
2021-12-05 15:54:53 +0100

    var to const in router.js

--- server/router.js
+++ server/router.js
@@ -1,25 +1,25 @@
 require("dotenv").config();
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
 const http = require("http");
 const server = http.createServer(app);
 const socketIo = require("socket.io");
 global.io = socketIo(server);
 
-var terms = require("./routes/terms.js");
+const terms = require("./routes/terms.js");
 app.use("/terms", terms);
 
-var streaming = require("./routes/streaming.js");
+const streaming = require("./routes/streaming.js");
 app.use("/stream", streaming);
 
-var maps = require("./routes/maps.js");
+const maps = require("./routes/maps.js");
 app.use("/map", maps);
 
-var index = require("./routes/index.js");
+const index = require("./routes/index.js");
 app.use("/index", index);
 
-var users = require("./routes/users.js");
+const users = require("./routes/users.js");
 app.use("/users", users);
 
 server.listen(8000, console.log("listening on 8000"));
