a9e83dda4d8d784913dfaea12dd47125e2e64a02
alessandro benassi trenta <alessandro.benassitrenta@gmail.com>
2022-01-04 02:32:39 +0100

    split maps.js in unit-testable functions, add tests for it also adjusting router.js

--- server/router.js
+++ server/router.js
@@ -8,13 +8,13 @@ const { Server } = require("socket.io");
 global.io = new Server(server);
 
 const terms = require("./routes/terms.js");
-app.use("/terms", terms);
+app.use("/terms", terms.router);
 
 const streaming = require("./routes/streaming.js");
 app.use("/stream", streaming);
 
 const maps = require("./routes/maps.js");
-app.use("/map", maps);
+app.use("/map", maps.router);
 
 const index = require("./routes/index.js");
 app.use("/index", index);
--- server/routes/maps.js
+++ server/routes/maps.js
@@ -5,28 +5,37 @@ const {
 const express = require("express");
 const cors = require("cors");
 const appOnlyClient = new TwitterApi(process.env.ADVANCED_BEARER);
-const client = appOnlyClient.readOnly;
+const mapsClient = appOnlyClient.readOnly;
 
 const router = express.Router();
 router.use(cors());
 
-router.get("/geo-keyword", async (req, res) => {
+async function searchGeo(client, req) {
   const range = req.query.range;
   const coords = JSON.parse(req.query.position);
   const keyword = req.query.keyword;
-  const results = await client.v2.search(
-    `${keyword} point_radius:[${coords.lng} ${coords.lat} ${
-      range / 1000
-    }km] has:geo`,
-    {
-      expansions: ["author_id"],
-      "tweet.fields": ["created_at", "public_metrics", "text", "geo"],
-      "user.fields": ["username", "name", "profile_image_url"],
-      max_results: 50,
-    }
-  );
-  console.log(results);
-  res.send(results.data);
+
+  return (await client.v2.search(
+      `${keyword} point_radius:[${coords.lng} ${coords.lat} ${
+          range / 1000
+      }km] has:geo`,
+      {
+        expansions: ["author_id"],
+        "tweet.fields": ["created_at", "public_metrics", "text", "geo"],
+        "user.fields": ["username", "name", "profile_image_url"],
+        max_results: 50,
+      }
+  )).data;
+}
+
+router.get("/geo-keyword", async (req, res) => {
+  let result = await searchGeo(mapsClient, req);
+
+  console.log("data");
+  console.log(result.data);
+  console.log("/data");
+
+  res.send(result);
 });
 
-module.exports = router;
+module.exports = { router, searchGeo };
