9b56d4064c4b471ff35f7ac2feeed091f2640708
fabio <fabiomirza@outlook.com>
2022-01-06 16:45:20 +0100

    temove .at()

--- server/routes/sentiment.js
+++ server/routes/sentiment.js
@@ -6,18 +6,15 @@
  * @property {int} totalCount - Total tweets counter
  */
 
- /**
-  * @typedef {object} SentimentTweets
-  * @property {int} positiveCount - Positive tweets counter
-  * @property {int} negativeCount - Negative tweets counter
-  * @property {int} sentimentCount - (Positive + negative) tweets counter
-  * @property {int} totalCount - Total tweets counter
-  */
+/**
+ * @typedef {object} SentimentTweets
+ * @property {int} positiveCount - Positive tweets counter
+ * @property {int} negativeCount - Negative tweets counter
+ * @property {int} sentimentCount - (Positive + negative) tweets counter
+ * @property {int} totalCount - Total tweets counter
+ */
 
-
-const {
-    TwitterApi
-} = require("twitter-api-v2");
+const { TwitterApi } = require("twitter-api-v2");
 
 const express = require("express");
 const cors = require("cors");
@@ -43,35 +40,35 @@ usa l'oggetto rec e fa le call alle API di twitter per trovare
 i tweet positivi, negativi e totali
 */
 async function searchCounts(req, client = sentimentClient) {
-    let keyword = req.query.keyword;
+  let keyword = req.query.keyword;
 
-    try{
-        let positiveTweets = await client.v2.tweetCountRecent(
-            `${keyword} (happy OR exciting OR excited OR favorite OR fav OR amazing OR lovely OR incredible) -horrible -worst -sucks -bad -disappointing`,
-            {"granularity": "day"}
-        );
+  try {
+    let positiveTweets = await client.v2.tweetCountRecent(
+      `${keyword} (happy OR exciting OR excited OR favorite OR fav OR amazing OR lovely OR incredible) -horrible -worst -sucks -bad -disappointing`,
+      { granularity: "day" }
+    );
 
-        let negativeTweets = await client.v2.tweetCountRecent(
-            `${keyword} (horrible OR worst OR sucks OR bad OR disappointing) -happy -exciting -excited -favorite -fav -amazing -lovely -incredible`,
-            {"granularity": "day"}
-        );
+    let negativeTweets = await client.v2.tweetCountRecent(
+      `${keyword} (horrible OR worst OR sucks OR bad OR disappointing) -happy -exciting -excited -favorite -fav -amazing -lovely -incredible`,
+      { granularity: "day" }
+    );
 
-        let totalTweets = await client.v2.tweetCountRecent(
-            `${keyword} -is:retweet`,
-            {"granularity": "day"}
-        );
+    let totalTweets = await client.v2.tweetCountRecent(
+      `${keyword} -is:retweet`,
+      { granularity: "day" }
+    );
 
-        let counts = {}
-        counts.positiveTweets = positiveTweets;
-        counts.negativeTweets = negativeTweets;
-        counts.totalTweets = totalTweets;
+    let counts = {};
+    counts.positiveTweets = positiveTweets;
+    counts.negativeTweets = negativeTweets;
+    counts.totalTweets = totalTweets;
 
-        return counts;
-    } catch (e) {
-        console.error(e);
-    }
+    return counts;
+  } catch (e) {
+    console.error(e);
+  }
 
-    return undefined;
+  return undefined;
 }
 
 /**
@@ -84,43 +81,42 @@ async function searchCounts(req, client = sentimentClient) {
  *  @returns {Result} - Returns a Result object.
  *
  */
-async function sentimentCount(counts){
-    if(counts === undefined){
-        return undefined;
-    }
-    let positiveTweets = counts.positiveTweets;
-    let negativeTweets = counts.negativeTweets;
-    let totalTweets = counts.totalTweets;
+async function sentimentCount(counts) {
+  if (counts === undefined) {
+    return undefined;
+  }
+  let positiveTweets = counts.positiveTweets;
+  let negativeTweets = counts.negativeTweets;
+  let totalTweets = counts.totalTweets;
 
-    const result = {};
-    result.days = [];
+  const result = {};
+  result.days = [];
 
-    let posCount = 0;
-    let negCount = 0;
-    let totCount = 0;
+  let posCount = 0;
+  let negCount = 0;
+  let totCount = 0;
 
-    for (let i = 0; i < 8; i++) {
-        let day = {};
-        day.date = positiveTweets.data.at(i).start;
-        day.pos_count = positiveTweets.data.at(i).tweet_count;
-        day.neg_count = negativeTweets.data.at(i).tweet_count;
-        day.tot_count = totalTweets.data.at(i).tweet_count;
-        result.days.push(day);
+  for (let i = 0; i < 8; i++) {
+    let day = {};
+    day.date = positiveTweets.data[i].start;
+    day.pos_count = positiveTweets.data[i].tweet_count;
+    day.neg_count = negativeTweets.data[i].tweet_count;
+    day.tot_count = totalTweets.data[i].tweet_count;
+    result.days.push(day);
 
+    posCount += positiveTweets.data[i].tweet_count;
+    negCount += negativeTweets.data[i].tweet_count;
+    totCount += totalTweets.data[i].tweet_count;
+  }
 
-        posCount += positiveTweets.data.at(i).tweet_count;
-        negCount += negativeTweets.data.at(i).tweet_count;
-        totCount += totalTweets.data.at(i).tweet_count;
-    }
+  result.positiveCount = posCount;
+  result.negativeCount = negCount;
+  result.sentimentCount = posCount + negCount;
+  result.totalCount = totCount;
 
-    result.positiveCount = posCount;
-    result.negativeCount = negCount;
-    result.sentimentCount = posCount + negCount;
-    result.totalCount = totCount;
+  // sentiment is a value in the range -2 -> +2, below is a table for calculating it
 
-    // sentiment is a value in the range -2 -> +2, below is a table for calculating it
-
-    /*
+  /*
     pos %   ->  val
     90-00   ->  +2
     66-89   ->  +1
@@ -129,38 +125,38 @@ async function sentimentCount(counts){
     00-09   ->  -2
      */
 
-    let sentiment;
-    let sentimentName;
+  let sentiment;
+  let sentimentName;
 
-    let posPercentage = 100 * posCount / (posCount + negCount);
-    if(posPercentage > 90){
-        sentiment = 2;
-        sentimentName = "Very Positive";
-    }else if(posPercentage > 66){
-        sentiment = 1;
-        sentimentName = "Positive";
-    }else if(posPercentage > 33){
-        sentiment = 0;
-        sentimentName = "Neutral";
-    }else if(posPercentage > 10){
-        sentiment = -1;
-        sentimentName = "Negative";
-    }else{
-        sentiment = -2;
-        sentimentName = "Very Negative";
-    }
+  let posPercentage = (100 * posCount) / (posCount + negCount);
+  if (posPercentage > 90) {
+    sentiment = 2;
+    sentimentName = "Very Positive";
+  } else if (posPercentage > 66) {
+    sentiment = 1;
+    sentimentName = "Positive";
+  } else if (posPercentage > 33) {
+    sentiment = 0;
+    sentimentName = "Neutral";
+  } else if (posPercentage > 10) {
+    sentiment = -1;
+    sentimentName = "Negative";
+  } else {
+    sentiment = -2;
+    sentimentName = "Very Negative";
+  }
 
-    result.sentiment = sentiment;
-    result.sentimentName = sentimentName;
+  result.sentiment = sentiment;
+  result.sentimentName = sentimentName;
 
-    return result;
+  return result;
 }
 
 router.get("/", async (req, res) => {
-    let counts = await searchCounts(req);
-    let result = await sentimentCount(counts);
+  let counts = await searchCounts(req);
+  let result = await sentimentCount(counts);
 
-    res.send(result);
+  res.send(result);
 });
 
 module.exports = { router, searchCounts, sentimentCount };
