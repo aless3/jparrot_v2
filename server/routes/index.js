62559c3929b594200df515d2f076e6e474280080
Giovanni Basso <giovanni.basso@icloud.com>
2022-01-06 13:17:08 +0100

    added documentation, improved documentation support

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-/** GET home page. */
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     description: get the index.html file
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: req
+ *         description: request object from the client.
+ *         required: true
+ *       - name: res
+ *         description: response object from the server.
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: page served
+ */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
