36edc9f3fa179f61798a96a7087bfe5e6edb27dc
alessandro benassi trenta <alessandro.benassitrenta@gmail.com>
2022-01-06 21:20:58 +0100

    coverage aumentato

--- server/test/streaming.test.js
+++ server/test/streaming.test.js
@@ -9,77 +9,77 @@ const client = appOnlyClient.readOnly;
 const mockSocket = require('mock-socket');
 
 beforeEach(async () => {
-  await streaming.resetRules(client);
+    await streaming.resetRules(client);
 });
 
 test("check if getRules get the correct info", async () => {
-  let expectedRules = await client.v2.streamRules();
+    let expectedRules = await client.v2.streamRules();
 
-  let rules = await streaming.getRules(client);
+    let rules = await streaming.getRules(client);
 
-  // avoid error in case the date in the meta field is different (for example by some seconds)
-  expectedRules.meta = null;
-  rules.meta = null;
+    // avoid error in case the date in the meta field is different (for example by some seconds)
+    expectedRules.meta = null;
+    rules.meta = null;
 
-  expect(rules).toStrictEqual(expectedRules);
+    expect(rules).toStrictEqual(expectedRules);
 });
 
 test("check if setRules correctly sets the rules", async () => {
-  let expectedRules = ["covid", "bologna"];
+    let expectedRules = ["covid", "bologna"];
 
-  await streaming.setRules(expectedRules, client);
-  // let rules = await streaming.getRules(client);
-  const rules = await streaming.getRules(client);
+    await streaming.setRules(expectedRules, client);
+    // let rules = await streaming.getRules(client);
+    const rules = await streaming.getRules(client);
 
-  for (const rule in expectedRules) {
-    expect(rules.data).not.toBe(
-      expect.objectContaining({
-        id: expect.any(String),
-        value: rule,
-      })
-    );
-  }
+    for (const rule in expectedRules) {
+        expect(rules.data).not.toBe(
+            expect.objectContaining({
+                id: expect.any(String),
+                value: rule,
+            })
+        );
+    }
 });
 
 test("check if deleteRules successfully erases the rules", async () => {
-  let tempRules = ["covid", "bologna"];
-  let persistentRules = ["london"];
-  await streaming.setRules(tempRules.concat(persistentRules), client);
-  let currentRules = await streaming.getRules(client);
-  let ids = [];
+    let tempRules = ["covid", "bologna"];
+    let persistentRules = ["london"];
+    await streaming.setRules(tempRules.concat(persistentRules), client);
+    let currentRules = await streaming.getRules(client);
+    let ids = [];
 
-  currentRules.data.map((rule) => {
-    if (tempRules.includes(rule.value)) {
-      ids.push(rule.id);
-    }
-  });
+    currentRules.data.map((rule) => {
+        if (tempRules.includes(rule.value)) {
+            ids.push(rule.id);
+        }
+    });
 
-  await streaming.deleteRules(ids, client);
+    await streaming.deleteRules(ids, client);
 
-  currentRules = await streaming.getRules(client);
+    currentRules = await streaming.getRules(client);
 
-  expect(tempRules.includes(currentRules.data[0].value)).toBeFalsy();
-  expect(persistentRules.includes(currentRules.data[0].value)).toBeTruthy();
+    expect(tempRules.includes(currentRules.data[0].value)).toBeFalsy();
+    expect(persistentRules.includes(currentRules.data[0].value)).toBeTruthy();
 });
 
 test("check if resetRules successfully resets the rules", async () => {
-  await streaming.resetRules(client);
-  let tempRules = ["covid", "bologna"];
+    await streaming.resetRules(client);
+    let tempRules = ["covid", "bologna"];
 
-  await streaming.setRules(tempRules, client);
-  await streaming.resetRules(client);
+    await streaming.setRules(tempRules, client);
+    await streaming.resetRules(client);
 
-  let emptyRules = await streaming.getRules(client);
+    let emptyRules = await streaming.getRules(client);
 
-  expect(emptyRules.meta.result_count).toBe(0);
+    expect(emptyRules.meta.result_count).toBe(0);
 });
 
 test("reloadRules correctly reloads the rules", async () => {
-  let rules = ["covid", "bologna"];
+    let rules = ["covid", "bologna"];
 
-  let result = await streaming.reloadRules(rules, client);
+    let result = await streaming.reloadRules(rules, client);
 
-  expect(result).toBeTruthy();
+    expect(result).toBeTruthy();
 });
 
 test('startStream correctly creates the stream', async () => {
