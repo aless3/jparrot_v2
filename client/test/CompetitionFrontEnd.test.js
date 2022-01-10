3be0341635b280a49388f77f7d99be61896ea78e
alessandro benassi trenta <alessandro.benassitrenta@gmail.com>
2022-01-10 19:05:20 +0100

    CompetitionFrontEnd.js completed tests

---
+++ client/test/CompetitionFrontEnd.test.js
@@ -0,0 +1,57 @@
+import {shallow} from "enzyme";
+import CompetitionFrontEnd from "../src/components/CompetitionFrontEnd";
+import React from "react";
+import axios from "axios";
+
+test("check if CompetitionFrontEnd correctly creates the podium", async () => {
+    const competition = shallow(<CompetitionFrontEnd />);
+
+    expect(competition).toMatchSnapshot();
+});
+
+test("check if CompetitionFrontEnd correctly updates creates the podium", async () => {
+    await axios.get("http://localhost:8000/competition");
+    const competition = shallow(<CompetitionFrontEnd />);
+
+    expect(competition).toMatchSnapshot();
+});
+
+test("check if CompetitionFrontEnd correctly changes the keyword value", async () => {
+    const competition = shallow(<CompetitionFrontEnd />);
+
+    let hashtag = competition.find("#keywordText").at(0)
+
+    let newObj = {
+        target: {
+            value: "#test"
+        }
+    }
+    hashtag.simulate("change", newObj)
+
+    expect(competition).toMatchSnapshot();
+});
+
+test("check if CompetitionFrontEnd correctly changes the max_result value", async () => {
+    const competition = shallow(<CompetitionFrontEnd />);
+
+    let maxResults = competition.find("#keywordSelect").at(0)
+
+    let newObj = {
+        target: {
+            value: "200"
+        }
+    }
+    maxResults.simulate("change", newObj)
+
+    expect(competition).toMatchSnapshot();
+});
+
+test("check if CompetitionFrontEnd correctly responds to the button press", async () => {
+    const competition = shallow(<CompetitionFrontEnd />);
+
+    let maxResults = competition.find("Button").at(0)
+
+    maxResults.simulate("click")
+
+    expect(competition).toMatchSnapshot();
+});
