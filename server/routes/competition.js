e9035bdad9b5bd894c0be3fa2a0b7a790fea71d1
alessandro benassi trenta <alessandro.benassitrenta@gmail.com>
2022-01-11 19:23:56 +0100

    updated competition backend, updated tests

--- server/routes/competition.js
+++ server/routes/competition.js
@@ -12,33 +12,52 @@ function hasWhiteSpace(s) {
     return /\s/g.test(s);
 }
 
-async function searchReplies(req, client = competitionClient){
-    let baseQuery = "#competition #jparrot_v2 #uniboswe2021 is:reply ";
-    let max_results;
+function buildQuery(hashtag, correctAnswer = null) {
+    let query = "#competition #jparrot_v2 #uniboswe2021 is:reply ";
+    // build the no-answer query
+    query = query.concat(hashtag)
 
+    if(!correctAnswer){
+        return query
+    }
+
+    // add a white space after the hashtag
+    query = query.concat(' ')
+    query = query.concat(correctAnswer)
+
+    return query;
+}
+
+async function searchReplies(req, client = competitionClient){
+    let hashtag = req.query.hashtag;
+    // hashtag cannot have white spaces
+    if(hasWhiteSpace(hashtag)){
+        return undefined;
+    }
+    // if it is not a real hashtag, make it one adding the # symbol at the beginning
+    if(hashtag.charAt(0) !== '#'){
+        hashtag = '#'.concat(hashtag)
+    }
+
+    // if correctAnswer is set, use it, else undefined
+    let correctAnswer;
+    try {
+        correctAnswer = req.query.correctAnswer;
+    } catch (e) {
+        correctAnswer = undefined;
+    }
+
+    let max_results;
     // if max_results is set, use it, else use 200 as default
     try {
         max_results = req.query.max_results;
     } catch (e) {
-        max_results = 50;
+        max_results = 100;
     }
 
     try {
-        let hashtag = req.query.hashtag;
 
-        // cannot have white spaces
-        if(hasWhiteSpace(hashtag)){
-            return undefined;
-        }
-
-        // if it is not a hashtag, make it one adding the # symbol at the beginning
-        if(hashtag.charAt(0) !== '#'){
-            hashtag = '#'.concat(hashtag)
-        }
-
-        // build the correct query
-        let query = baseQuery;
-        query = query.concat(hashtag)
+        let query = buildQuery(hashtag, correctAnswer);
 
         const search = await client.v2.search(query, {
             expansions: ["author_id"],
@@ -49,8 +68,7 @@ async function searchReplies(req, client = competitionClient){
 
 
         // fetchLast twice to have all results
-        await search.fetchLast(max_results * 10);
-
+        await search.fetchLast(max_results * 2);
 
         return search.data;
     } catch (e) {
@@ -89,23 +107,78 @@ function updateLists(lists, newItem) {
     return lists;
 }
 
-function organizeReplies(replies) {
-    let errorResult = {}
-    errorResult.data = []
+function containsWrongsAnswers(text, wrongAnswers = []) {
+    for (const wrongAnswer of wrongAnswers) {
+        try {
+            if(text.includes(wrongAnswer)){
+                return true
+            }
+        }catch (e) {
+            return true
+        }
+    }
+    return false
+}
 
-    errorResult.includes = {}
-    errorResult.includes.users = []
+function extractIndices(replies, listIndices){
+    let result = {}
+    result.data = []
 
-    if(replies === undefined || replies.meta === undefined || replies.meta.result_count === 0){
+    result.includes = {}
+    result.includes.users = []
+
+    for (const i of listIndices) {
+        let tweet = replies.data[i];
+        if(tweet === undefined){
+            continue;
+        }
+
+        let user = replies.includes.users.filter(
+            (u) => u.id === tweet.author_id
+        );
+        user = user[0];
+
+        result.data.push(tweet)
+        if(!result.includes.users.includes(user)){
+            result.includes.users.push(user)
+        }
+    }
+
+    return result
+}
+
+function organizeAnswers(replies, wrongAnswers = []) {
+    let isWrongArray = Array.isArray(wrongAnswers)
+
+    if(!isWrongArray){
         return undefined;
     }
+
     try {
-        let result = {}
-        result.data = []
 
-        result.includes = {}
-        result.includes.users = []
+        let lists = {};
+        lists.listLikes = [-1, -1, -1, -1];
+        lists.listIndices = [-1, -1, -1, -1];
 
+        for (let i = 0; i < replies.data.length; i++) {
+            let reply = {};
+            reply.value = (new Date(replies.data[i].created_at)).getTime();
+            reply.index = i;
+
+            lists = updateLists(lists, reply)
+        }
+
+        let listIndices = lists.listIndices.reverse();
+
+        return extractIndices(replies, listIndices)
+    } catch (e) {
+        console.log(e)
+        return undefined;
+    }
+}
+
+function organizeCompetition(replies){
+    try {
         let lists = {};
         lists.listLikes = [-1, -1, -1, -1];
         lists.listIndices = [-1, -1, -1, -1];
@@ -120,24 +193,36 @@ function organizeReplies(replies) {
 
         let listIndices = lists.listIndices;
 
-        for (const i of listIndices) {
-            let tweet = replies.data[i];
-            if(tweet === undefined){
-                continue;
-            }
+        return extractIndices(replies, listIndices)
+    }catch (e) {
+        console.log(e)
+        return undefined;
+    }
+}
 
-            let user = replies.includes.users.filter(
-                (u) => u.id === tweet.author_id
-            );
-            user = user[0];
+function organizeReplies(replies, wrongAnswers = []) {
+    let errorResult = {}
+    errorResult.data = []
 
-            result.data.push(tweet)
-            if(!result.includes.users.includes(user)){
-                result.includes.users.push(user)
-            }
+    errorResult.includes = {}
+    errorResult.includes.users = []
+
+    if(!replies || !replies.meta || replies.meta.result_count === 0){
+        return errorResult;
+    }
+
+    try {
+        let result;
+        if(wrongAnswers === []){
+            result = organizeCompetition(replies)
+        }else {
+            result = organizeAnswers(replies, wrongAnswers)
+        }
+        if(!result){
+            result = errorResult
         }
 
-        return result;
+        return result
     }catch (e) {
         console.log(e)
         return errorResult;
@@ -148,7 +233,8 @@ function organizeReplies(replies) {
 router.get("/", async (req, res) => {
     let replies = await searchReplies(req);
     let result = organizeReplies(replies);
+
     res.send(result);
 });
 
-module.exports = { router, searchReplies, organizeReplies, hasWhiteSpace, updateLists };
+module.exports = { router, searchReplies, organizeReplies, organizeCompetition, organizeAnswers, hasWhiteSpace, updateLists, containsWrongsAnswers, buildQuery, extractIndices };
--- server/test/competition.test.js
+++ server/test/competition.test.js
@@ -19,12 +19,36 @@ test('check hasWhiteSpace correctly detects spaces', () => {
     expect(competition.hasWhiteSpace(s)).toBeTruthy();
 });
 
+test('check containsWrongsAnswers correctly detects wrong answers', () => {
+    let text = "old pond frog leaps in water's sound"
+    let wrong = ["the first cold shower"]
+    expect(competition.containsWrongsAnswers(text, wrong)).toBeFalsy();
+
+    wrong = ["the first cold shower", "old pond frog"]
+    expect(competition.containsWrongsAnswers(text, wrong)).toBeTruthy();
+
+    wrong = ["the first cold shower", "old pond frog!"]
+    expect(competition.containsWrongsAnswers(text, wrong)).toBeFalsy();
+});
+
+test('check buildQuery correctly builds the query', () => {
+    let hashtag = "#test"
+    let expectedQuery = "#competition #jparrot_v2 #uniboswe2021 is:reply #test";
+
+    expect(competition.buildQuery(hashtag)).toEqual(expectedQuery)
+
+
+    let correctAnswer = "the cat is on the table";
+    expectedQuery = "#competition #jparrot_v2 #uniboswe2021 is:reply #test the cat is on the table";
+
+    expect(competition.buildQuery(hashtag, correctAnswer)).toEqual(expectedQuery)
+});
+
 test('check updateLists returns the correctly updated object', () => {
     let lists = {
         listLikes: [-1, -1, -1, -1, -1],
         listIndices: [-1, -1, -1, -1, -1]
     }
-
 
     let newItem = {
         value: 6,
@@ -86,15 +110,86 @@ test('check updateLists returns the correctly updated object', () => {
     expect(lists).toEqual(expected)
 });
 
-test('check organizeReplies correctly organizes the replies correctly using stored blobs', () => {
-    const input = require('../blobs/organizeReplies_input.json');
-    const expectedOutput = require('../blobs/organizeReplies_output.json');
+test('check organizeCompetition correctly organizes the replies correctly using stored blobs', () => {
+    const input = require('../blobs/organizeCompetition_input.json');
+    const expectedOutput = require('../blobs/organizeCompetition_output.json');
 
-    let realOutput = competition.organizeReplies(input)
+    let realOutput = competition.organizeCompetition(input)
 
     expect(realOutput).toEqual(
         expect.objectContaining(expectedOutput)
     );
+});
+
+test('check extractIndices correctly extracts indices', () => {
+    const input = require('../blobs/organizeCompetition_input.json');
+    const expectedOutput = {
+        data: [input.data[0]],
+        includes: {
+            users: [input.includes.users[0]]
+        }
+    }
+    // 267138741
+    let realOutput = competition.extractIndices(input, [0])
+
+    expect(realOutput.data).toEqual(expectedOutput.data)
+    expect(realOutput.includes.users).toEqual(expectedOutput.includes.users)
+});
+
+// test('check organizeCompetition correctly organizes the replies correctly using stored blobs', () => {
+//     const input = require('../blobs/organizeCompetition_input.json');
+//     const expectedOutput = require('../blobs/organizeCompetition_output.json');
+//
+//     let realOutput = competition.organizeCompetition(input)
+//
+//     expect(realOutput).toEqual(
+//         expect.objectContaining(expectedOutput)
+//     );
+// });
+
+test('check organizeReplies correctly returns an empty result on error', () => {
+    let errorResult = {}
+    errorResult.data = []
+
+    errorResult.includes = {}
+    errorResult.includes.users = []
+
+    let output = competition.organizeReplies(null, null)
+
+    expect(output).toEqual(errorResult)
+});
+
+test('check organizeReplies correctly correctly computes data for competition using stored blobs', () => {
+    const input = require('../blobs/organizeAnswers_input.json');
+    const wa = require('../blobs/organizeAnswers_wa.json');
+
+    let realOutput = competition.organizeAnswers(input, wa)
+
+    let date0 = new Date(realOutput.data[0].created_at)
+    let date1 = new Date(realOutput.data[1].created_at)
+    let date2 = new Date(realOutput.data[2].created_at)
+    let date3 = new Date(realOutput.data[3].created_at)
+
+    expect(date0.getTime() < date1.getTime()).toBeTruthy()
+    expect(date1.getTime() < date2.getTime()).toBeTruthy()
+    expect(date2.getTime() < date3.getTime()).toBeTruthy()
+});
+
+test('check organizeReplies correctly correctly computes data with answers', () => {
+    const input = require('../blobs/organizeCompetition_input.json');
+    const wa = require('../blobs/organizeAnswers_wa.json');
+
+    let realOutput = competition.organizeAnswers(input, wa)
+
+
+    let date0 = new Date(realOutput.data[0].created_at)
+    let date1 = new Date(realOutput.data[1].created_at)
+    let date2 = new Date(realOutput.data[2].created_at)
+    let date3 = new Date(realOutput.data[3].created_at)
+
+    expect(date0.getTime() < date1.getTime()).toBeTruthy()
+    expect(date1.getTime() < date2.getTime()).toBeTruthy()
+    expect(date2.getTime() < date3.getTime()).toBeTruthy()
 });
 
 test('check if searchReplies can correctly access the info needed', async () => {
