0e7d5134977f2d8955e1771c03f269ae3d11c763
Jason Freamon <jasonfreamon@protonmail.com>
2022-01-18 15:08:06 +0100

    Aggiunti altri test in CompetitionFrontEnd

--- client/test/CompetitionFrontEnd.test.js
+++ client/test/CompetitionFrontEnd.test.js
@@ -1,57 +1,87 @@
-import {shallow} from "enzyme";
+import { mount, shallow } from "enzyme";
 import CompetitionFrontEnd from "../src/components/CompetitionFrontEnd";
 import React from "react";
 import axios from "axios";
 
 test("check if CompetitionFrontEnd correctly creates the podium", async () => {
-    const competition = shallow(<CompetitionFrontEnd />);
+  const competition = shallow(<CompetitionFrontEnd />);
 
-    expect(competition).toMatchSnapshot();
+  expect(competition).toMatchSnapshot();
 });
 
 test("check if CompetitionFrontEnd correctly updates creates the podium", async () => {
-    await axios.get("http://localhost:8000/competition");
-    const competition = shallow(<CompetitionFrontEnd />);
+  await axios.get("http://localhost:8000/competition");
+  const competition = shallow(<CompetitionFrontEnd />);
 
-    expect(competition).toMatchSnapshot();
+  expect(competition).toMatchSnapshot();
 });
 
 test("check if CompetitionFrontEnd correctly changes the keyword value", async () => {
-    const competition = shallow(<CompetitionFrontEnd />);
+  const competition = shallow(<CompetitionFrontEnd />);
 
-    let hashtag = competition.find("#keywordText").at(0)
+  let hashtag = competition.find("#keywordText").at(0);
 
-    let newObj = {
-        target: {
-            value: "#test"
-        }
-    }
-    hashtag.simulate("change", newObj)
+  let newObj = {
+    target: {
+      value: "#test",
+    },
+  };
+  hashtag.simulate("change", newObj);
 
-    expect(competition).toMatchSnapshot();
+  expect(competition).toMatchSnapshot();
 });
 
 test("check if CompetitionFrontEnd correctly changes the max_result value", async () => {
-    const competition = shallow(<CompetitionFrontEnd />);
+  const competition = shallow(<CompetitionFrontEnd />);
 
-    let maxResults = competition.find("#keywordSelect").at(0)
+  let maxResults = competition.find("#keywordSelect").at(0);
 
-    let newObj = {
-        target: {
-            value: "200"
-        }
-    }
-    maxResults.simulate("change", newObj)
+  let newObj = {
+    target: {
+      value: "200",
+    },
+  };
+  maxResults.simulate("change", newObj);
 
-    expect(competition).toMatchSnapshot();
+  expect(competition).toMatchSnapshot();
 });
 
 test("check if CompetitionFrontEnd correctly responds to the button press", async () => {
-    const competition = shallow(<CompetitionFrontEnd />);
+  const competition = shallow(<CompetitionFrontEnd />);
 
-    let maxResults = competition.find("Button").at(0)
+  let maxResults = competition.find("Button").at(0);
 
-    maxResults.simulate("click")
+  maxResults.simulate("click");
 
-    expect(competition).toMatchSnapshot();
+  expect(competition).toMatchSnapshot();
+});
+
+test("check if CompetitionFrontEnd correctly responds to the mostliked button press", async () => {
+  const competition = shallow(<CompetitionFrontEnd />);
+
+  let maxResults = competition.find("Button").at(1);
+
+  maxResults.simulate("click");
+
+  expect(competition).toMatchSnapshot();
+});
+
+test("check if CompetitionFrontEnd correctly responds to the open-ended button press", async () => {
+  const competition = shallow(<CompetitionFrontEnd />);
+
+  let maxResults = competition.find("Button").at(2);
+
+  maxResults.simulate("click");
+
+  expect(competition).toMatchSnapshot();
+});
+
+test("check if CompetitionFrontEnd correctly responds to the multiple choise button press", async () => {
+  const competition = shallow(<CompetitionFrontEnd />);
+
+  let maxResults = competition.find("Button").at(3);
+
+  maxResults.simulate("click");
+
+  expect(competition).toMatchSnapshot();
 });
