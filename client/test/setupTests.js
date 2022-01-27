65264c801564473cb9722ba4c2166d06d3775711
fabio <fabiomirza@outlook.com>
2022-01-27 19:00:54 +0100

    correct test failure

--- client/test/setupTests.js
+++ client/test/setupTests.js
@@ -40,3 +40,16 @@ axios.get.mockImplementation((url) => {
   }
   return Promise.resolve(result);
 });
+
+var createElementNSOrig = global.document.createElementNS;
+global.document.createElementNS = function (namespaceURI, qualifiedName) {
+  if (
+    namespaceURI === "http://www.w3.org/2000/svg" &&
+    qualifiedName === "svg"
+  ) {
+    var element = createElementNSOrig.apply(this, arguments);
+    element.createSVGRect = function () {};
+    return element;
+  }
+  return createElementNSOrig.apply(this, arguments);
+};
