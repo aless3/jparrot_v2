75e96c97435bc9c5c184aee1783283c76fe29f68
fabio <fabiomirza@outlook.com>
2022-01-27 15:01:17 +0100

    add test to keyword

--- server/routes/keyword.js
+++ server/routes/keyword.js
@@ -1,6 +1,4 @@
-const {
-    TwitterApi
-} = require("twitter-api-v2");
+const { TwitterApi } = require("twitter-api-v2");
 
 const express = require("express");
 const appOnlyClient = new TwitterApi(process.env.ADVANCED_BEARER);
@@ -9,26 +7,25 @@ const keywordClient = appOnlyClient.readOnly;
 const router = express.Router();
 
 async function searchKeyword(req, client = keywordClient) {
-    let keyword = req.query.keyword;
-    try {
-        return (await client.v2.search(
-            `${keyword}`,
-            {
-                expansions: ["author_id"],
-                "tweet.fields": ["created_at", "public_metrics", "text"],
-                "user.fields": ["username", "name", "profile_image_url"],
-                max_results: 50,
-            }
-        )).data;
-    }catch (e) {
-        console.error(e);
-        return undefined;
-    }
+  let keyword = req.query.keyword;
+  try {
+    return (
+      await client.v2.search(`${keyword}`, {
+        expansions: ["author_id"],
+        "tweet.fields": ["created_at", "public_metrics", "text"],
+        "user.fields": ["username", "name", "profile_image_url"],
+        max_results: 50,
+      })
+    ).data;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
 }
 
 router.get("/", async (req, res) => {
-    let result = await searchKeyword(req);
-    res.send(result);
+  let result = await searchKeyword(req);
+  res.send(result);
 });
 
 module.exports = { router, searchKeyword };
