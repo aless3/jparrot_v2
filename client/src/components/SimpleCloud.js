df84f9431721dab9d0eeb0e62ac42acec875f6cc
fabio <fabiomirza@outlook.com>
2022-01-30 16:25:20 +0100

    word cloud color

--- client/src/components/SimpleCloud.js
+++ client/src/components/SimpleCloud.js
@@ -5,12 +5,12 @@ import { TagCloud } from "react-tagcloud";
 export function SimpleCloud({ values, setter }) {
   return (
     <TagCloud
-      style={{ width: 1000, textAlign: "center" /*color: "#5BD199"*/ }}
-      //disableRandomColor={true}
+      style={{ width: 1000, textAlign: "center", color: "#5BD199" }}
       minSize={10}
       maxSize={60}
       tags={values}
       shuffle={true}
+      disableRandomColor={true}
       onClick={(tag) => setter(tag.value)}
       /*colorOptions={{
         luminosity: "light",
